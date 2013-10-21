7eb3b2b27beae73a82f9a9b2328f0baa32d70f30
Brent Tubbs <brent.tubbs@gmail.com>
2013-10-21 14:22:39 -0700

    add RPC interface and runners

---
+++ runners/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+    name='vr.runners',
+    namespace_packages=['vr'],
+    version='0.0.1',
+    author='Brent Tubbs',
+    author_email='brent.tubbs@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'vr.common>=2.15.2',
+        'requests>=1.0.4',
+        'PyYAML>=3.10',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vrun_precise = vr.runners.precise:main',
+        ]
+    },
+    description=('Command line tools to launch procs.'),
+)
