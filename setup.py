061909b4153b62cca826e14ba4b891d1cb57bebb
Jason R. Coombs <jaraco@jaraco.com>
2015-12-07 08:55:38 -0500

    Only invoke setup when setup script is executed

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+params = dict(
     name='vr.runners',
     namespace_packages=['vr'],
     version='2.8.1',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     include_package_data=True,
     url='https://bitbucket.org/yougov/vr.runners',
     install_requires=[
@@ -25,3 +25,6 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
 )
+
+if __name__ == '__main__':
+    setuptools.setup(**params)
